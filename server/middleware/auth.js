bc6f0661c269381f3339d33e2bbd6950514c36c5
manibharathi <manibharathi8005@gmail.com>
2025-12-26 14:04:51 +0530

    fix: update case-sensitive import in auth.js

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -1,28 +1,28 @@
 
-import User from "../models/User.js"
+import User from "../models/user.js"
 import jwt from "jsonwebtoken"
 
 //middleware to protect routes
 
 
-export const protectedRoute=async(req,res,next)=>{
-    try{
-        const token=req.headers.token
-        const decoded=jwt.verify(token,process.env.JWT_SECRET)
+export const protectedRoute = async (req, res, next) => {
+    try {
+        const token = req.headers.token
+        const decoded = jwt.verify(token, process.env.JWT_SECRET)
 
 
-        const user=await User.findById(decoded.userId).select("-password")
-        if (!user){
-            return res.json({success:false,message:"User not found"})
+        const user = await User.findById(decoded.userId).select("-password")
+        if (!user) {
+            return res.json({ success: false, message: "User not found" })
         }
-        req.user=user
+        req.user = user
         next()
 
-    }catch(error){
+    } catch (error) {
         console.log(error.message);
-        
+
         res.json({
-            success:false,message: error.message                                                                      
+            success: false, message: error.message
         })
     }
 }
