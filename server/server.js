5a23dcd1d3c83ee3757e0c316aee70b8eba7431c
manibharathi <manibharathi8005@gmail.com>
2025-12-26 14:56:14 +0530

    fix: update console log to use FRONTEND_URLS plural

--- server/server.js
+++ server/server.js
@@ -81,7 +81,7 @@ await connectDB();
 const PORT = process.env.PORT || 5000;
 server.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
-  console.log(`Allowed CORS Origin: ${FRONTEND_URL}`);
+  console.log(`Allowed CORS Origins: ${FRONTEND_URLS.join(", ")}`);
 });
 
 export default app;
